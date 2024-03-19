1200079a442e60be2abf2405c54a40809035c64e
yonolie <153516949+yonolie@users.noreply.github.com>
2024-03-19 15:05:38 +0100

    Add files via upload

---
+++ progammering eindopdracht/js/index.js
@@ -0,0 +1,18 @@
+const lightBulb = document.querySelector("#icon");
+const clickSound = document.querySelector("#clickSound");
+let iconStatus = false;
+
+function switchicon() {
+    if (iconStatus == true) {
+        lightBulb.src = "content/min.png";
+        iconStatus = false;
+    } else {
+        lightBulb.src = "content/plus.png";
+        iconStatus = true;
+    }
+    // Geluid afspelen wanneer er op de lamp wordt geklikt
+    clickSound.play();
+    gap : 10 px
+}
+
+lightBulb.addEventListener('click', switchicon);
